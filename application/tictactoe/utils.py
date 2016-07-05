1a5af4fd32314e4cb3c9dff370e2ce4510493845
Deephan <deephan@gmail.com>
2016-07-05 08:47:12 -0400

    chore(typo): Correcting Typo

--- application/tictactoe/utils.py
+++ application/tictactoe/utils.py
@@ -2,7 +2,7 @@
 
     utils.py
 
-    Utilities module for building the status messages and parsing incoming slack response_str
+    Utilities module for building the status messages and parsing incoming slack response
 
     Note: This file is currently empty. Work to be done.
 
